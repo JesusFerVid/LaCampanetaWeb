a13c3d547dad439c5b8bf2327468cf2c949c9ca2
JesusFerVid <jesusfervid94@gmail.com>
2022-06-03 10:15:43 +0200

    Carga de contenido dinámico correcta
    Ya se carga contenido de manera dinámica en formato XML.
    Como JS no puede cargar archivos locales, los datos XML están almacenados dentro de constantes, como código.

---
+++ js/data.js
@@ -0,0 +1,55 @@
+const landscapes_xml = `
+	<landscapes>
+		<landscape id='landscape_1'>			
+			<name>Paisaje 1</name>
+			<desc>Introduzca texto elaborado...</desc>
+			<image>img/landscapes/default.jpg</image>
+		</landscape>
+		<landscape id='landscape_2'>
+			<name>Paisaje 2</name>
+			<desc>Introduzca texto elaborado...</desc>
+			<image>img/landscapes/default.jpg</image>
+		</landscape>
+		<landscape id='landscape_3'>
+			<name>Paisaje 3</name>
+			<desc>Introduzca texto elaborado...</desc>
+			<image>img/landscapes/default.jpg</image>
+		</landscape>
+		<landscape id='landscape_4'>
+			<name>Paisaje 4</name>
+			<desc>Introduzca texto elaborado...</desc>
+			<image>img/landscapes/default.jpg</image>
+		</landscape>
+		<landscape id='landscape_5'>
+			<name>Paisaje 5</name>
+			<desc>Introduzca texto elaborado...</desc>
+			<image>img/landscapes/default.jpg</image>
+		</landscape>
+		<landscape id='landscape_6'>
+			<name>Paisaje 6</name>
+			<desc>Introduzca texto elaborado...</desc>
+			<image>img/landscapes/default.jpg</image>
+		</landscape>
+	</landscapes>
+`
+
+const dishes_xml = `
+	<dishes>
+		<dish id="dish_1">
+			<name>Arroz con costra</name>
+			<desc>Un plato delicioso, típico de la zona. Algunos lo llaman arroz tapado, otros tortilla de arroz... yo lo llamo delicia.</desc>
+			<rating>5</rating>
+			<recipe>
+				<ingredients>
+					<ingredient>Ingrediente 1</ingredient>
+					<ingredient>Ingrediente 2</ingredient>
+					<ingredient>Ingrediente 3</ingredient>
+				</ingredients>
+				<instructions>
+					<p>Este plato se hace de esta manera...</p>
+				</instructions>
+			</recipe>
+			<image>img/dishes/arroz-con-costra.jpg</image>
+		</dish>
+	</dishes>
+`;
--- js/script.js
+++ js/script.js
@@ -23,19 +23,124 @@ function toggleNav() {
 	}
 }
 
+// Carga las miniaturas de los paisajes
+function loadLandscapes() {
+	let container = document.getElementById('landscapes-container');
+	let id, src, name, content;
+	let xml = new DOMParser().parseFromString(landscapes_xml, 'text/xml');
+	let landscapes = xml.getElementsByTagName('landscape');
+
+	for (let landscape of landscapes) {
+		id = landscape.getAttribute('id');
+		src = landscape.getElementsByTagName('image')[0].innerHTML;
+		name = landscape.getElementsByTagName('name')[0].innerHTML;
+		content = `
+			<article id="${id}" onclick="goToImgDetail(this)">
+				<div class="image-wrapper"><img src="${src}" alt="${name}"></div>
+				<div class="article-info"><h3 class="article-title">${name}</h3></div>
+			</article>
+		`;
+		container.innerHTML += content;
+	}
+}
+
 // Nos lleva a la página de detalle de imagen
 function goToImgDetail(element) {
-	sessionStorage.imgSrc = element.getElementsByTagName('img')[0].attributes.src.value;
-	sessionStorage.imgAlt = element.getElementsByTagName('img')[0].attributes.alt.value;
+	sessionStorage.setItem('id', element.getAttribute('id'));
 	window.location.href = 'image-detail.html';
 }
 
-// Carga la imagen que queremos ver en la página detalle
+// Carga la información de la imagen que queremos ver en la página detalle
 function loadImgDetail(){
-	let title = document.getElementById('image-title');
-	let img = document.getElementById('main-image-wrapper').firstChild;
-	console.log('Image details');
-	title.innerHTML = sessionStorage.imgAlt;
-	img.attributes.src.value = sessionStorage.imgSrc;
-	img.attributes.alt.value = sessionStorage.imgAlt;
+	let id = sessionStorage.getItem('id');
+	let xml = new DOMParser().parseFromString(landscapes_xml, 'text/xml');
+	let landscape = xml.getElementById(id);
+	
+	let name =  landscape.getElementsByTagName('name')[0].innerHTML;
+	let src = landscape.getElementsByTagName('image')[0].innerHTML;
+	let desc = landscape.getElementsByTagName('desc')[0].innerHTML;
+
+	document.getElementById('name').innerHTML = name;
+	document.getElementById('main-image-wrapper').firstChild.setAttribute('src', src);
+	document.getElementById('main-image-wrapper').firstChild.setAttribute('alt', name);
+	document.getElementById('desc').innerHTML = desc;
+}
+
+// Carga las miniaturas de los platos
+function loadDishes() {
+	let container = document.getElementById('dishes-container');
+	let id, name, desc, rating, stars, src, content;
+	let xml = new DOMParser().parseFromString(dishes_xml, 'text/xml');
+	let dishes = xml.getElementsByTagName('dish');
+
+	for (let dish of dishes) {
+		id = dish.getAttribute('id');
+		name = dish.getElementsByTagName('name')[0].innerHTML
+		desc = dish.getElementsByTagName('desc')[0].innerHTML
+		rating = dish.getElementsByTagName('rating')[0].innerHTML
+		stars = generateDishStars(rating);
+		console.log(stars);
+		src = dish.getElementsByTagName('image')[0].innerHTML
+		content = `
+			<article id="${id}" onclick="goToDishDetail(this)">
+				<div class="image-wrapper"><img src="${src}" alt="${name}"></div>
+				<div class="article-info">
+					<h3 class="article-title">${name}</h3>
+					<span class="article-desc">${desc}</span>
+					<span class="article-rating with-icon">Valoración: ${stars}</span>
+				</div>
+			</article>
+		`;
+		container.innerHTML += content;
+	}
+}
+
+// Nos lleva a la página de detalle de plato
+function goToDishDetail(element) {
+	sessionStorage.setItem('id', element.getAttribute('id'));
+	window.location.href = 'dish-detail.html';
+}
+
+// Carga la información del plato que queremos ver en la página detalle
+function loadDishDetail() {
+	let id = sessionStorage.getItem('id');
+	let xml = new DOMParser().parseFromString(dishes_xml, 'text/xml');
+	let dish = xml.getElementById(id);
+	
+	let src = dish.getElementsByTagName('image')[0].innerHTML;
+	let name = dish.getElementsByTagName('name')[0].innerHTML;
+	let desc = dish.getElementsByTagName('desc')[0].innerHTML;
+	let rating = generateDishStars(dish.getElementsByTagName('rating')[0].innerHTML);
+	let ingredients = generateDishIngredients(dish.getElementsByTagName('ingredient'));
+	let instructions = generateDishInstructions(dish.getElementsByTagName('p'));
+	
+	document.getElementById('main-image-wrapper').firstChild.setAttribute('src', src);
+	document.getElementById('main-image-wrapper').firstChild.setAttribute('alt', name);
+	document.getElementById('name').innerHTML = name;
+	document.getElementById('desc').innerHTML = desc;
+	document.getElementById('rating').innerHTML = rating;
+	document.getElementById('ingredients').innerHTML = ingredients;
+	document.getElementById('instructions').innerHTML = instructions;
+}
+
+// Devuelve el HTML con el número indicado de estrellas
+function generateDishStars(rating) {
+	let i, output = "";
+	for (i = 0; i < rating; i++ ) {
+		output += `<span class="material-icons">star</span>`;
+	}
+	for (; i < 5; i++ ) {
+		output += `<span class="material-icons">star_border</span>`;
+	}
+	return output;
+}
+
+// Devuelve el HTML de los ingredientes
+function generateDishIngredients(ingredients) {
+	return "TODO";
+}
+
+// Devuelve el HTML de las instrucciones
+function generateDishInstructions(instructions) {
+	return "TODO";
 }
